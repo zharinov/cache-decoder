a9bfe5b7669b76fe0da3f45f95468d499ea5239f
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-08-30 16:39:48 +0300

    Usability enhancements

--- src/app.js
+++ src/app.js
@@ -1,24 +1,58 @@
 import decompress from "brotli/decompress";
 
 function decompressPayload(input) {
-  const decompressed = decompress(Buffer.from(input, "base64"));
+  let massagedInput = input;
+  try {
+    const parsed = JSON.parse(input);
+    if (typeof parsed?.payload === "string") {
+      massagedInput = parsed.payload;
+    }
+  } catch (err) {
+    // no-op
+  }
+
+  const decompressed = decompress(Buffer.from(massagedInput, "base64"));
   const text = new TextDecoder().decode(decompressed);
   const json = JSON.parse(text);
   return JSON.stringify(json, undefined, 2);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector("button");
+  const copied = document.querySelector("#copied");
+  const reset = document.querySelector("#reset");
   const input = document.querySelector("#input-text");
   const output = document.querySelector("#output-text");
-  button?.addEventListener("click", function () {
-    const inputText = input?.value;
 
-    if (!inputText) {
-      return;
+  input.addEventListener("input", function () {
+    if (!input.value.trim()) {
+      reset.setAttribute("disabled", true);
+    } else {
+      const inputText = input?.value;
+
+      if (!inputText) {
+        return;
+      }
+
+      reset.removeAttribute("disabled");
+
+      try {
+        output.value = decompressPayload(inputText);
+        output.select();
+        navigator.clipboard.writeText(output.value);
+        copied.removeAttribute("hidden");
+        setTimeout(function () {
+          copied.setAttribute("hidden", true);
+        }, 3000);
+      } catch (err) {
+        output.value = String(err);
+      }
     }
+  });
 
-    const outputText = decompressPayload(inputText);
-    output.value = outputText;
-  }); 
+  reset.addEventListener("click", function () {
+    input.value = "";
+    output.value = "";
+    reset.setAttribute("disabled", true);
+    input.focus();
+  });
 });
